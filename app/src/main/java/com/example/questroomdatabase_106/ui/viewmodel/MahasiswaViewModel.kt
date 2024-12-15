76032e56bf83337fdb95223a2539109a9bf441b2
Bayhaqiindra <bayhaqi30072004.com>
2024-12-15 19:23:58 +0700

    Menambahkan fungsi ekstensi `toMahasiswaEntity` untuk mengubah data dari `MahasiswaEvent` menjadi objek `Mahasiswa` yang sesuai dengan entity database.

--- app/src/main/java/com/example/questroomdatabase_106/ui/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/questroomdatabase_106/ui/viewmodel/MahasiswaViewModel.kt
@@ -1,5 +1,17 @@
 package com.example.questroomdatabase_106.ui.viewmodel
 
+import com.example.questroomdatabase_106.data.entity.Mahasiswa
+
+//menyimpan input form ke dalam entity
+fun MahasiswaEvent.toMahasiswaEntity(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    jenisKelamin = jenisKelamin,
+    alamat = alamat,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
 //data class variable yang menyimpan data input form
 data class MahasiswaEvent(
     val nim: String = "",
