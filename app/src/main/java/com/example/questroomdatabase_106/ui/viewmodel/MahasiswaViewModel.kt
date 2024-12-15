84001542d415c1fc39b472b5271490868438690c
Bayhaqiindra <bayhaqi30072004.com>
2024-12-15 19:20:21 +0700

    Menambahkan data class `MahasiswaEvent` didalam file Mahasiswa ViewModeluntuk menyimpan data input dari form Mahasiswa, termasuk atribut seperti NIM, nama, jenis kelamin, alamat, kelas, dan angkatan dengan nilai default kosong.

---
+++ app/src/main/java/com/example/questroomdatabase_106/ui/viewmodel/MahasiswaViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.questroomdatabase_106.ui.viewmodel
+
+//data class variable yang menyimpan data input form
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String ="",
+    val jenisKelamin: String ="",
+    val alamat: String ="",
+    val kelas: String ="",
+    val angkatan: String ="",
+)
